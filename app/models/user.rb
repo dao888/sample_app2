df7e3f3dcec0484efbc53f0364d9a378e1384be4
robert lu <dao888@gmail.com>
2013-03-10 14:19:59 -0700

    Finish Sign-in-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ActiveRecord::Base
 attr_accessible :name, :email, :password, :password_confirmation
 has_secure_password
 before_save { |user| user.email = email.downcase }
+before_save :create_remember_token
+
+#before_save { |user| user.remember_token = "weproiytwoire" }
+
+
 validates :name, presence: true, length: { maximum: 50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true,
@@ -27,10 +32,11 @@ validates :password, presence: true, length: { minimum: 6 }
 validates :password_confirmation, presence: true
 
 
-private
-def create_remember_token
-self.remember_token = SecureRandom.urlsafe_base64
-end
+ private
+
+ def create_remember_token
+ self.remember_token = SecureRandom.urlsafe_base64
+ end
 
 
 
